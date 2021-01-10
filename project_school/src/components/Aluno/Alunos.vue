c25d16325c92ea166b34f08c76dba8f06d511aab
Andre Santos <andreluis.cosme@hotmail.com>
2021-01-10 13:45:03 -0300

    Adicionando Título

---
+++ project_school/src/components/Aluno/Alunos.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <titulo texto="Aluno"/>
+    <div>
+      <input type="text" placeholder="Nome do Aluno" v-model="nome" @keyup.enter="addAluno()"/>
+      <button class="btn btn_input" @click="addAluno()">Adicionar</button>
+    </div>
+    <table>
+      <thead>
+        <th>Mat.</th>
+        <th>Nome</th>
+        <th>Opções</th>
+      </thead>
+      <tbody v-if="alunos.length">
+        <tr v-for="(aluno, index) in alunos" :key="index">
+          <td>{{index+1}}</td>
+          <!--<td>{{aluno.id}}</td>-->
+          <td>{{aluno.nome}} {{aluno.sobrenome}}</td>
+          <td>
+            <button class="btn btn_danger" @click="remover(aluno)">Remover</button>
+          </td>
+        </tr>
+      </tbody>
+      <tfoot v-else>
+          Nenhum Aluno Encontrado
+      </tfoot>
+    </table>
+  </div>
+</template>
+
+<script>
+import Titulo from '../_share/Titulo';
+
+export default {
+  components:{
+    Titulo
+  },
+  data() {
+    return {
+      titulo: "Aluno",
+      nome: '',
+      alunos: [
+        {id: 1, nome: 'Marcus', sobrenome: 'Xavier'},
+        {id: 2, nome: 'Lucas', sobrenome: 'Amaral'},
+        {id: 3, nome: 'Paulo', sobrenome: 'Henrique'}
+        ]
+    };
+  },
+  props: {},
+  methods: {
+    addAluno() {
+      let _aluno = {
+        nome: this.nome
+      }
+      this.alunos.push(_aluno);
+      this.nome = '';
+    },
+    remover(aluno){
+      let indice = this.alunos.indexOf(aluno);
+      this.alunos.splice(indice, 1);
+    }
+  },
+};
+</script>
+
+<style scoped>
+input{
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  color: #687f7f;
+  display: inline;
+}
+.btn_input{
+  border: 0px;
+  padding: 20px;
+  font-size: 1.3em;
+  display: inline;
+  background-color: rgb(116, 115, 115);
+}
+.btn_input:hover{
+  padding: 20px;
+  margin: 0px;
+  border: 0;
+}
+</style>
