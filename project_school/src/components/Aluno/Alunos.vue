df75b6a0bd85fbc75316f3cb0fd651c60c6fb056
Andre Santos <andreluis.cosme@hotmail.com>
2021-01-10 16:11:36 -0300

    Commit Final - PROJETO VUE CLI

--- project_school/src/components/Aluno/Alunos.vue
+++ project_school/src/components/Aluno/Alunos.vue
@@ -13,8 +13,7 @@
       </thead>
       <tbody v-if="alunos.length">
         <tr v-for="(aluno, index) in alunos" :key="index">
-          <td>{{index+1}}</td>
-          <!--<td>{{aluno.id}}</td>-->
+          <td>{{aluno.id}}</td>
           <td>{{aluno.nome}} {{aluno.sobrenome}}</td>
           <td>
             <button class="btn btn_danger" @click="remover(aluno)">Remover</button>
@@ -39,25 +38,36 @@ export default {
     return {
       titulo: "Aluno",
       nome: '',
-      alunos: [
-        {id: 1, nome: 'Marcus', sobrenome: 'Xavier'},
-        {id: 2, nome: 'Lucas', sobrenome: 'Amaral'},
-        {id: 3, nome: 'Paulo', sobrenome: 'Henrique'}
-        ]
+      alunos: []
     };
+  },
+  created(){
+    this.$http.get('http://localhost:3000/alunos')
+      .then(res => res.json())
+      .then(alunos => this.alunos = alunos)
   },
   props: {},
   methods: {
     addAluno() {
       let _aluno = {
-        nome: this.nome
+        nome: this.nome,
+        sobrenome: ""
       }
-      this.alunos.push(_aluno);
-      this.nome = '';
+
+    this.$http.post('http://localhost:3000/alunos', _aluno)
+      .then(res => res.json())
+      .then(aluno => {
+        this.alunos.push(aluno);
+        this.nome = '';
+      })
+
     },
     remover(aluno){
-      let indice = this.alunos.indexOf(aluno);
-      this.alunos.splice(indice, 1);
+      this.$http.delete(`http://localhost:3000/alunos/${aluno.id}`)
+        .then(() => {
+          let indice = this.alunos.indexOf(aluno);
+          this.alunos.splice(indice, 1);
+        })
     }
   },
 };
